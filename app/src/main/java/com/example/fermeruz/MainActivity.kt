6b9fee63725d7785c751e67738097acfd477bb0d
ShoxruxRavshanbekov <ravshanbekov001@gmail.com>
2021-12-21 14:11:27 +0500

    Ravshanbekov Sh 21.12.2021 14:10

--- app/src/main/java/com/example/fermeruz/MainActivity.kt
+++ app/src/main/java/com/example/fermeruz/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.fermeruz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        fab.setOnClickListener {
+            startActivity(Intent(this, loginPage::class.java))
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.mConsult -> startActivity(Intent(this, loginPage::class.java))
+                R.id.mChat -> startActivity(Intent(this, loginPage::class.java))
+                R.id.mPerson -> startActivity(Intent(this, loginPage::class.java))
+            }
+            true
+
+        }
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
